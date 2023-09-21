43b3778c97c9012e9cc4024eb4d78235db06cf72
wojtish <20014605+wojtish@users.noreply.github.com>
2023-09-21 13:06:56 +0200

    Update godzina_przypomnienia.js

--- godzina_przypomnienia.js
+++ godzina_przypomnienia.js
@@ -3,6 +3,6 @@ moment.locale('pl');
 var K = moment(R).format('LLLL');
 var T = moment(R).format('HH:mm');
 entry().set("godzina przypomnienia", T)
-
+entry().set("godzina przypomnienia archiwum", entry().field("godzina przypomnienia archiwum") + " " + T)
 message(K)
 //message(T)
